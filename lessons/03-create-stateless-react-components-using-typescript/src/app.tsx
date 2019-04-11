e72b2e18972fe135355bc2c298b801180b699c47
Sam Grinis <samgrinis@Sams-MBP.lan>
2019-04-11 11:46:32 -0400

    updated all dependencies

---
+++ lessons/03-create-stateless-react-components-using-typescript/src/app.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+type AppProps = { message: string }
+const App: React.SFC<AppProps> = ({ message }) => <div>{message}</div>;
+
+ReactDOM.render(
+    <div>Hello world</div>,
+    document.getElementById('root')
+  );
+  
+  
